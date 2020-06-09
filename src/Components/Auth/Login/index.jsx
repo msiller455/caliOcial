b73bafbd22f070a4a57b8282768d12eea0971a8c
Michael <msiller455@gmail.com>
2020-06-08 17:14:24 -0700

    refactors hooks on signup and login

--- src/Components/Auth/Login/index.jsx
+++ src/Components/Auth/Login/index.jsx
@@ -1,64 +1,15 @@
-import React, { useState } from 'react';
-import { AuthContainer, AuthWindow, AuthForm, ErrorMessage } from '../style'
+import React from 'react';
+import useForm from '../hooks/useForm'
+import { AuthContainer, AuthWindow, AuthForm } from '../style'
 
 const Login = () => {
+
+    const login = () => {
+        console.log(values)
+    }
+
+    const { values, handleChange, handleSubmit } = useForm(login)
     
-    const [ formData, setFormData ] = useState({
-        email: "",
-        password: ""
-    })
-
-    const [ errorMessages, setErrorMessages ] = useState({
-        email: "",
-        password: "",
-        error: ""
-    })
-
-    const handleChange = (event) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value
-        })
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        checkFormData()
-    }
-
-    const checkFormData = () => {
-        emailCheck(formData.email)
-        passwordCheck(formData.password)
-    }
-
-    const emailCheck = (email) => {
-        if(!email.length) {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                email: "Please enter your email"
-            }))
-        } else {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                email: ""
-            }))
-        }
-    }
-
-    const passwordCheck = (password) => {
-        if(!password.length) {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                password: "Please enter your password"
-            }))
-        } else {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                password: ""
-            }))
-        }
-    }
-
     return (
         <AuthContainer>
             <AuthWindow page="login">
@@ -67,13 +18,11 @@ const Login = () => {
                     <label>
                         Email:
                     </label>
-                    <ErrorMessage>{errorMessages.email}</ErrorMessage>
-                    <input type="email" name="email" value={formData.email} onChange={handleChange}/>
+                    <input type="email" name="email" value={values.email || ''} onChange={handleChange}/>
                     <label>
                         Password:
                     </label>
-                    <ErrorMessage>{errorMessages.password}</ErrorMessage>
-                    <input type="password" name="password" value={formData.password} onChange={handleChange}/>
+                    <input type="password" name="password" value={values.password || ''} onChange={handleChange}/>
                     <button type="submit">
                         Login
                     </button>
--- src/Components/Auth/SignUp/index.jsx
+++ src/Components/Auth/SignUp/index.jsx
@@ -1,116 +1,14 @@
-import React, { useState } from 'react';
-import { AuthContainer, AuthWindow, AuthForm, ErrorMessage } from '../style'
+import React from 'react';
+import useForm from '../hooks/useForm'
+import { AuthContainer, AuthWindow, AuthForm } from '../style'
 
 const SignUp = () => {
 
-    const [ formData, setFormData ] = useState({
-        username: '',
-        name: '',
-        email: '',
-        passwordOne: '',
-        passwordTwo: '',
-    })
-
-    const [ errorMessages, setErrorMessages ] = useState({
-        username: '',
-        name: '',
-        email: '',
-        passwordOne: '',
-        passwordTwo: '',
-        error: ''
-    })
-
-
-    const handleChange = (event) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value
-        })
+    const signUp = () => {
+        console.log(values)
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        checkFormData()
-    }
-
-    const checkFormData = () => {
-        usernameCheck(formData.username)
-        nameCheck(formData.name)
-        emailCheck(formData.email)
-        passwordOneCheck(formData.passwordOne)
-        passwordTwoCheck(formData.passwordOne, formData.passwordTwo)
-    }
-
-    const usernameCheck = (username) => {
-        if(!username.length) {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                username: "Please enter your username"
-            }))
-        } else {// ADD CHECK IF USERNAME ALREADY EXISTS
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                username: ""
-            }))
-        }
-    }
-
-    const nameCheck = (name) => {
-        if(!name.length) {
-            setErrorMessages((prevState)=> ({
-                ...prevState,
-                name: "Please enter your name"
-            }))
-        } else {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                name: ""
-            }))
-        }
-    }
-
-    const emailCheck = (email) => {
-        if(!email.length) {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                email: "Please enter your email"
-            }))
-        } else {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                email: ""
-            }))
-        }
-    }
-
-    const passwordOneCheck = (passwordOne) => {
-        const pwRegEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
-        if(!passwordOne.match(pwRegEx)) {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                passwordOne: "Password must be at least 8 characters and include at least 1 uppercase letter and 1 number"
-            }))
-        } else {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                passwordOne: ""
-            }))
-        }
-    }
-
-    const passwordTwoCheck = (passwordOne, passwordTwo) => {
-        if(passwordOne !== passwordTwo) {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                passwordTwo: "Passwords must match"
-            }))
-        } else {
-            setErrorMessages((prevState) => ({
-                ...prevState,
-                passwordTwo: ""
-            }))
-        }
-    }
+    const { values, handleChange, handleSubmit } = useForm(signUp)
 
     return (
         <AuthContainer>
@@ -120,38 +18,23 @@ const SignUp = () => {
                     <label>
                         Username:
                     </label>
-                    <ErrorMessage>
-                        {errorMessages.username}
-                    </ErrorMessage>
-                    <input type="text" name="username" value={formData.username} onChange={handleChange}/>
+                    <input type="text" name="username" value={values.username || ''} onChange={handleChange} required />
                     <label>
                         Name:
                     </label>
-                    <ErrorMessage>
-                        {errorMessages.name}
-                    </ErrorMessage>
-                    <input type="text" name="name" value={formData.name} onChange={handleChange}/>
+                    <input type="text" name="name" value={values.name || ''} onChange={handleChange} required />
                     <label>
                         Email:
                     </label>
-                    <ErrorMessage>
-                        {errorMessages.email}
-                    </ErrorMessage>
-                    <input type="email" name="email" value={formData.email} onChange={handleChange}/>
+                    <input type="email" name="email" value={values.email || ''} onChange={handleChange} required />
                     <label>
                         Password:
                     </label>
-                    <ErrorMessage>
-                        {errorMessages.passwordOne}
-                    </ErrorMessage>
-                    <input type="password" name="passwordOne" value={formData.passwordOne} onChange={handleChange}/>
+                    <input type="password" name="passwordOne" value={values.passwordOne || ''} onChange={handleChange} required />
                     <label>
                         Confirm Password:
                     </label>
-                    <ErrorMessage>
-                        {errorMessages.passwordTwo}
-                    </ErrorMessage>
-                    <input type="password" name="passwordTwo"value={formData.passwordTwo} onChange={handleChange}/>
+                    <input type="password" name="passwordTwo"value={values.passwordTwo || ''} onChange={handleChange} required />
                     <button type="submit">
                         Create Profile
                     </button>

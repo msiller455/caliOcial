25b06c6d4b2f426ada811630c26067c8f3f14320
Michael <msiller455@gmail.com>
2020-06-20 17:53:28 -0700

    adds useLocation to clean up detail page

--- src/Components/CleanUps/CleanUpDetail/index.jsx
+++ src/Components/CleanUps/CleanUpDetail/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom'
 
-const CleanUpDetail = (props) => {
-    
+const CleanUpDetail = () => {
+    const cleanUp = useLocation().state.cleanUp
+
     return (
         <div>
             <h1>This is CleanUpDetail</h1>

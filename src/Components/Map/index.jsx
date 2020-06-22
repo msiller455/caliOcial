fd84a2aaf67cab3ab14dbc4c3f53ae2ad0ac56da
Michael <msiller455@gmail.com>
2020-06-22 16:31:30 -0700

    adds deployment stuff as well as map marker

--- src/Components/Map/index.jsx
+++ src/Components/Map/index.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons' 
 import { MapContainer, NavControl } from './style'
+import {theme} from '../../constants/theme'
 
 const Map = (props) => {
     const [ viewport, setViewport ] = useState({
@@ -10,15 +13,23 @@ const Map = (props) => {
         longitude: props.long,
         zoom: 14
     })
+    const [ isHovered, setIsHovered ] = useState(false)
 
     return (
         <MapContainer>
-            <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API}>
+            <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API} onViewportChange={(viewport) => setViewport(viewport)}>
                 <NavControl>
                     <NavigationControl />
                 </NavControl>
                 <Marker latitude={props.lat} longitude={props.long}>
-                    HELLO    
+                     <FontAwesomeIcon 
+                        icon={faMapMarker}
+                        size="1x"
+                        style={{transition: "all 0.25s"}}
+                        color={isHovered ? theme.hoverColor : '#000'}
+                        onMouseOver={()=> setIsHovered(true)}
+                        onMouseOut={() => setIsHovered(false)}
+                     />
                 </Marker> 
             </ReactMapGL>
         </MapContainer>
